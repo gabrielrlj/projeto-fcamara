6c8f0ac6f3ff1c17c475e3561a39111454c15f95
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-23 15:19:45 -0300

    Criacao do projeto backend inicial

---
+++ Backend/doacao-material/src/main/java/com/squad11/doacao/DoacaoMaterialApplication.java
@@ -0,0 +1,13 @@
+package com.squad11.doacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoacaoMaterialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoacaoMaterialApplication.class, args);
+	}
+
+}
---
+++ Backend/doacao-material/src/test/java/com/squad11/doacao/DoacaoMaterialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad11.doacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoacaoMaterialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
