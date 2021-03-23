e6fdf5b0a7db93dcf08d21db220e614f02015d7c
Luskas8 <gnomo1049@gmail.com>
2021-03-23 17:26:01 -0300

    Renomeaçao das tabs iniciais

--- Frontend/app/navigation/BottomTabNavigator.tsx
+++ Frontend/app/navigation/BottomTabNavigator.tsx
@@ -5,9 +5,9 @@ import * as React from 'react';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import TabOneScreen from '../screens/LoginScreen';
+import TabTwoScreen from '../screens/RegisterScreen';
+import { BottomTabParamList, LoginParamList, RegisterParamList } from '../types';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -16,18 +16,18 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="Login"
       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
+        name="Login"
+        component={LoginNavigator}
         options={{
           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
+        name="Register"
+        component={RegisterNavigator}
         options={{
           tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
         }}
@@ -44,30 +44,30 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
+const LoginStack = createStackNavigator<LoginParamList>();
 
-function TabOneNavigator() {
+function LoginNavigator() {
   return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
+    <LoginStack.Navigator>
+      <LoginStack.Screen
+        name="LoginScreen"
         component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
+        options={{ headerTitle: 'Entrar' }}
       />
-    </TabOneStack.Navigator>
+    </LoginStack.Navigator>
   );
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const RegisterStack = createStackNavigator<RegisterParamList>();
 
-function TabTwoNavigator() {
+function RegisterNavigator() {
   return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
+    <RegisterStack.Navigator>
+      <RegisterStack.Screen
+        name="RegisterScreen"
         component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
+        options={{ headerTitle: 'Registrar' }}
       />
-    </TabTwoStack.Navigator>
+    </RegisterStack.Navigator>
   );
 }
