ff428f6f0b46ed2efae3ababa737aa52cbce8dac
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-23 16:12:03 -0300

    configurando banco de dados H2 e criando entidade Endereco

---
+++ Backend/doacao-material/src/main/java/com/squad11/doacao/entities/Endereco.java
@@ -0,0 +1,114 @@
+package com.squad11.doacao.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Endereco {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String logradouro;
+	private String numero;
+	private String complemento;
+	private String bairro;
+	private String cep;
+	private String cidade;
+	private String estado;
+	
+
+	
+	public Endereco() {
+		
+	}
+	
+	public Endereco(Long id, String logradouro, String numero, String complemento, String bairro, String cep,
+			String cidade, String estado) {
+		
+		this.id = id;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogradouro() {
+		return logradouro;
+	}
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Endereco [id=");
+		builder.append(id);
+		builder.append(", logradouro=");
+		builder.append(logradouro);
+		builder.append(", numero=");
+		builder.append(numero);
+		builder.append(", complemento=");
+		builder.append(complemento);
+		builder.append(", bairro=");
+		builder.append(bairro);
+		builder.append(", cep=");
+		builder.append(cep);
+		builder.append(", cidade=");
+		builder.append(cidade);
+		builder.append(", estado=");
+		builder.append(estado);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+}
