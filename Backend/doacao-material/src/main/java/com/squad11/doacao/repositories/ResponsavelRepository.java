09064a3b56acb02a3d4ff9b62fd0466115634acc
Gabriel Jardim <gabrielrlj99@outlook.com>
2021-03-24 10:51:41 -0300

    criado o ResponsavelRepository

---
+++ Backend/doacao-material/src/main/java/com/squad11/doacao/repositories/ResponsavelRepository.java
@@ -0,0 +1,9 @@
+package com.squad11.doacao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.squad11.doacao.entities.Responsavel;
+
+public interface ResponsavelRepository extends JpaRepository<Responsavel, Long>{
+
+}
