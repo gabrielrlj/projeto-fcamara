1247c560f0ecdfd299f733f6fadfa60ca2a1c434
Luskas8 <gnomo1049@gmail.com>
2021-03-24 18:06:52 -0300

    Reinicio do frontend

--- Frontend/app/App.tsx
+++ Frontend/app/App.tsx
@@ -4,7 +4,6 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -15,8 +14,8 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <Home />
+        {/* <StatusBar /> */}
       </SafeAreaProvider>
     );
   }
