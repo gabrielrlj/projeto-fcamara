d8bd99e86a6f1d3d45dc7c40620ff3ab17414855
Fernanda Ferraz <fernanda_ferraz2015@outlook.com>
2021-03-25 11:51:48 -0300

    criacao do repositorio de materiais

---
+++ Backend/doacao-material/src/main/java/com/squad11/doacao/repositories/MaterialRepository.java
@@ -0,0 +1,13 @@
+package com.squad11.doacao.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.squad11.doacao.entities.Material;
+
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material, Long>{
+	
+
+}
