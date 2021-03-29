451e505920ea805a2484e4a127654f0f2c09b692
Luskas8 <gnomo1049@gmail.com>
2021-03-29 17:01:29 -0300

    Tipagem de parametros, exclussao de comentario e identaçao

--- Frontend/app/screens/authScreens/DefaultLogin.tsx
+++ Frontend/app/screens/authScreens/DefaultLogin.tsx
@@ -1,8 +1,7 @@
-import { useNavigation } from '@react-navigation/core';
-import {StackNavigationProp } from '@react-navigation/stack'
+import { StackNavigationProp } from '@react-navigation/stack'
 import React, { useState, useEffect } from 'react'
 import { RouteProp } from '@react-navigation/native'
-import {RootStackParamList } from '../../types';
+import { RootStackParamList } from '../../types';
 import { StyleSheet, View, Text, TouchableOpacity, TextInput, Image } from 'react-native'
 import IconLogin from '../../assets/images/elo-escolar-1.png'
 
@@ -17,63 +16,57 @@ interface LoginProps {
 }
 
 export default function DefaultLogin({ navigation, route }: LoginProps) {
+  // hook - constante criada nome email e setEmail function que define valor de email. Usestate é o estado atual
+  // state se inicia com null
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function RegisterDonorScreen() {
+    navigation.navigate('Dashboard', { currentScreen: 'RegisterDonor' });
+  }
 
 
-  // useEffect(() => {
-  //     navigation.reset({
-  //       index: 0,
-  //       routes: [route],
-  //     });
-  //   }, []);
+  // const é constante 
+  // handleChange recebe e que recebe o valor setado de Email com o valor de e
+  const handleChangeEmail = (e: string) => {
+    setEmail(e);
+  }
+  const handleChangePassword = (e: string) => {
+    setPassword(e);
+  }
 
-    function RegisterDonorScreen() {
-      navigation.navigate('Dashboard', { currentScreen: 'RegisterDonor'});
-    }
+  return (
+    <View style={styles.container}>
+      <Image source={IconLogin} style={styles.image} />
+      <TextInput placeholder='E-mail' style={styles.input}
+        autoFocus={true} keyboardType='email-address'
+        value={email}
+        //toda vez que mudar o texto chama a função handleChangeEmail e muda o valor da variável com o valor digitado
+        onChangeText={e => {
+          handleChangeEmail(e);
+        }}>
+      </TextInput>
+      <TextInput placeholder='Senha' style={styles.input}
+        autoFocus={true} secureTextEntry={true}
+        value={password}
+        onChangeText={e => {
+          handleChangePassword(e);
+        }}>
+      </TextInput>
 
+      <TouchableOpacity onPress={() => { }} style={styles.buttom}>
+        <Text style={styles.buttomText}>Entrar</Text>
+      </TouchableOpacity>
 
-    // const é constante 
-    // handleChange recebe e que recebe o valor setado de Email com o valor de e
-    const handleChangeEmail = e => {
-      setEmail(e);
-    }
-    const handleChangePassword = e => {
-      setPassword(e);
-    }
-    // hook - constante criada nome email e setEmail function que define valor de email. Usestate é o estado atual
-    // state se inicia com null
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+      <TouchableOpacity onPress={RegisterDonorScreen} style={styles.buttom}>
+        <Text style={styles.buttomText}>Cadastre-se como doador</Text>
+      </TouchableOpacity>
 
-    return (
-      <View style={styles.container}>
-        <Image source={IconLogin} style={styles.image}/> 
-        <TextInput placeholder='E-mail' style={styles.input}
-          autoFocus={true} keyboardType='email-address'
-          value={email}
-          //toda vez que mudar o texto chama a função handleChangeEmail e muda o valor da variável com o valor digitado
-          onChangeText={e => {
-            handleChangeEmail(e);
-          }}>
-        </TextInput>
-        <TextInput placeholder='Senha' style={styles.input}
-          autoFocus={true} secureTextEntry={true}
-          value={password}
-          onChangeText={e => {
-            handleChangePassword(e);
-          }}>
-        </TextInput>
-
-        <TouchableOpacity onPress={() => { }} style={styles.buttom}>
-          <Text style={styles.buttomText}>Entrar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={RegisterDonorScreen} style={styles.buttom}>
-          <Text style={styles.buttomText}>Cadastre-se como doador</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => { }} style={styles.buttom}>
-          <Text style={styles.buttomText}>Cadastre-se como beneficiado</Text>
-        </TouchableOpacity>
-      </View>
-    )
+      <TouchableOpacity onPress={() => { }} style={styles.buttom}>
+        <Text style={styles.buttomText}>Cadastre-se como beneficiado</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
