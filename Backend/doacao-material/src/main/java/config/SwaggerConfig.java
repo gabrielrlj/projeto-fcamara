a63e5ef3e09615ddee94e1603ab245399a4d1017
Fernanda Ferraz <fernanda_ferraz2015@outlook.com>
2021-03-31 22:34:07 -0300

    Alterando nome do controller

--- Backend/doacao-material/src/main/java/config/SwaggerConfig.java
+++ Backend/doacao-material/src/main/java/config/SwaggerConfig.java
@@ -9,35 +9,25 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 	@Bean
-	public Docket docket(){
-	return new Docket(DocumentationType.SWAGGER_2).select()
-			.apis( RequestHandlerSelectors.basePackage
-					("generation.org.blogPessoal.controller") )
-					.paths(PathSelectors.any())
-					.build()
-					.apiInfo(apiInfo());
-					}
-					private ApiInfo apiInfo(){
-					return new ApiInfoBuilder()
-					.title("Elo Escolar")
-					.description("API do Projeto Elo Escolar")
-					.version("1.0")
-					.contact(contact())
-					.build();
-					}
-					private Contact contact(){
-					return new Contact("Fernanda Ferraz, Gabriel Jardim, Jésica Mariane, Lucas Silva",
-					"https://github.com/gabrielrlj/projeto-fcamara",
-					"Projeto Elo Escolar para a Fcamara");
-					}
-					}
-	
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.squad11.doacao.controllers"))
+				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
+	}
 
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Elo Escolar").description("API do Projeto Elo Escolar").version("1.0")
+				.contact(contact()).build();
+	}
 
-
+	private Contact contact() {
+		return new Contact("Fernanda Ferraz, Gabriel Jardim, Jésica Mariane, Lucas Silva",
+				"https://github.com/gabrielrlj/projeto-fcamara", "Projeto Elo Escolar para a Fcamara");
+	}
+}
