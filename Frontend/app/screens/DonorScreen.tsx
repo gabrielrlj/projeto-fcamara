a8f5f17c6d9eca44afadbb6217e43f74f0f5ce8b
Lucas Anjos <42415782+luskas8@users.noreply.github.com>
2021-04-03 14:02:42 -0300

    GET dados dos dependentes

--- Frontend/app/screens/DonorScreen.tsx
+++ Frontend/app/screens/DonorScreen.tsx
@@ -1,6 +1,6 @@
 import { RouteProp, useNavigation } from '@react-navigation/core';
 import { createStackNavigator, StackNavigationProp } from '@react-navigation/stack';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Button from '../components/Button';
@@ -48,11 +48,23 @@ function DonorDashboard({ navigation }: DonorProps ) {
     function handleConfirmationDonation() {
         navigation.navigate('ConfirmationDonation');
     }
+    
+    useEffect(() => {
+        api.get('/doadores/depenpendes')
+            .then(({data}) => {
+                console.log(data);
+                // quardar esse data numa variável
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
 
     return (
       <View style={{flex: 1, overflow: 'visible'}}>
         <Text>Página inicial {username}</Text>
         <ScrollView style={styles.studentsContainer}>
+            {/* mapear a variavel do data aqui criando um componente Student pra cada */}
             <Student />
             <Student />
             <Student />
